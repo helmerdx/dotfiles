3e50e71dcbcdf1dec20895bf29c6343f6e271902
Helmer <11061182+helmerdavila@users.noreply.github.com>
2026-03-20 15:12:47 -0400

    Installing omz from sh

---
+++ run_once_install-omz.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Installs Oh My Zsh on first run. OMZ manages its own updates via auto-updater.
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+
+  if [ "$SHELL" != "$(which zsh)" ]; then
+    chsh -s "$(which zsh)"
+  fi
+fi
