c9a6465ee7818204f8c74b2303989dafb21aeee9
Helmer <11061182+helmerdavila@users.noreply.github.com>
2026-07-24 23:07:05 -0400

    Lazyvim: disable conceal level

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LazyVim's own json_conceal fix (config/autocmds.lua) registers on VeryLazy,
+-- which can fire after the FileType event for a file opened directly from the
+-- command line (nvim foo.json), leaving quotes concealed on first open.
+-- Registering it here, before lazy.nvim/VeryLazy, closes that race.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
